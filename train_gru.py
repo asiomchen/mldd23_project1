627b850de13147391e1c4468d28849751071a57f
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-06-29 13:22:41 +0200

    update train script

--- train_gru.py
+++ train_gru.py
@@ -6,7 +6,6 @@ from gru.cce import CCE, ConsciousCrossEntropy
 from vectorizer import SELFIESVectorizer, determine_alphabet
 from split import scaffold_split
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 import os
 import torch
@@ -23,9 +22,8 @@ vectorizer = SELFIESVectorizer(alphabet, pad_to_len=128)
 data_path = './GRU_data/combned_dataset.parquet'
 dataset = pd.read_parquet(data_path)
 
-train_size = 0.9
+train_size = 0.8
 
-print("Executing scaffold split...")
 train_df, val_df = scaffold_split(dataset, train_size)
 print("Scaffold split complete")
 
@@ -36,11 +34,11 @@ print("Dataset size:", len(dataset))
 print("Train size:", len(train_dataset))
 print("Val size:", len(val_dataset))
 
-batch_size = 512
+batch_size = 64
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, drop_last=True)
 val_loader = DataLoader(val_dataset, shuffle=False, batch_size=batch_size, drop_last=True)
 
-run_name = 'v3_server_test'
+run_name = '100_epochs'
 
 if not os.path.isdir(f'./models/{run_name}'):
     os.mkdir(f'./models/{run_name}')
@@ -50,9 +48,9 @@ val_df.to_parquet(f'./models/{run_name}/val_dataset.parquet')
 # Set hyperparameters
 encoding_size = 512
 hidden_size = 512
-num_layers = 3
+num_layers = 1
 learn_rate = 0.0005
-dropout = 0.2 # dropout must be equal 0 if num_layers = 1
+dropout = 0 # dropout must be equal 0 if num_layers = 1
 teacher_ratio = 0.5
 
 # Init model
@@ -62,27 +60,9 @@ model = EncoderDecoder(
     hidden_size=hidden_size,
     num_layers=num_layers,
     dropout=dropout,
-    teacher_ratio = teacher_ratio).to(device)
+    teacher_ratio = teacher_ratio)
 
-def train(model, train_loader, val_loader, vectorizer):
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    EPOCHS = 20
-
-    # Define dataframe for training progess
-    epochs_range = range(1,EPOCHS+1)
-    metrics = pd.DataFrame(columns=['epoch', 'train_loss', 'val_loss']);
-    metrics['epoch'] = epochs_range
-    
-    # Init example printer
-    printer = ExamplePrinter('./models/{run_name}/val_dataset.parquet', val_loader, num_examples=25)
-
-    # Define loss function and optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
-    criterion = CCE()
-
-    # wandb config and init
+ # wandb config and init
     config = dict()
     config['learning rate'] = learn_rate
     config['encoding size'] = model.encoding_size
@@ -95,19 +75,38 @@ def train(model, train_loader, val_loader, vectorizer):
     config['Dropout'] = model.decoder.dropout
     config['Batch size'] = batch_size
     config['teacher_ratio'] = teacher_ratio
-    wandb.init(project="gmum-servers", config=config)
+    wandb.init(project="gmum-servers", config=config, dir='./tmp')
+
+model = nn.DataParallel(model).to(device)
+
+def train(model, train_loader, val_loader, vectorizer):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    EPOCHS = 100
+
+    # Define dataframe for training progess
+    epochs_range = range(1,EPOCHS+1)
+    metrics = pd.DataFrame(columns=['epoch', 'train_loss', 'val_loss']);
+    metrics['epoch'] = epochs_range
+
+    # Init example printer
+    printer = ExamplePrinter('./models/{run_name}/val_dataset.parquet', val_loader, num_examples=25)
+
+    # Define loss function and optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
+    criterion = CCE()
 
     print("Starting Training of GRU")
     print(f"Device: {device}")
-    
     samples = []
-    
+
     # Start training loop
     for epoch in epochs_range:
         print(f'Epoch: {epoch}')
         epoch_loss = 0
         model.train()
-        for batch_idx, (X,y) in enumerate(tqdm(train_loader)):
+        for batch_idx, (X,y) in enumerate(train_loader):
             X = X.to(device)
             y = y.to(device)
             optimizer.zero_grad()
@@ -123,14 +122,14 @@ def train(model, train_loader, val_loader, vectorizer):
         metrics_dict = {'epoch': epoch,
                         'train_loss': avg_loss,
                         'val_loss': val_loss}
-        wandb.log(metrics_dict)
+        #wandb.log(metrics_dict)
 
         # Update metrics df
         metrics.loc[len(metrics)] = metrics_dict
-        
-        save_path = f"./models/{run_name}/epoch_{epoch}.pt"
-        torch.save(model.state_dict(),save_path)
-        
+        if (epoch % 10 == 0):
+            save_path = f"./models/{run_name}/epoch_{epoch}.pt"
+            torch.save(model.state_dict(),save_path)
+        metrics.to_csv(f"./models/{run_name}/metrics.csv")
         new_samples = printer(model)
         samples.append(new_samples)
 
