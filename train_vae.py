ec00263c51cda2f5769b36eb9751957fb0fe4c34
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-06-30 10:16:09 +0200

    Update train_vae.py

--- train_vae.py
+++ train_vae.py
@@ -18,6 +18,8 @@ batch_size = 32
 
 test_size = 0.8
 
+EPOCHS = 500
+
 full_path = './GRU_data/combined_dataset.parquet'
 
 run_name = 'vae_500epochs'
@@ -38,8 +40,8 @@ import torch.utils.data as data
 train_dataset, val_dataset = data.random_split(dataset, [test_size, 1-test_size])
 
 from torch.utils.data import DataLoader
-train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, drop_last=True, num_workers=4)
-val_loader = DataLoader(val_dataset, shuffle=False, batch_size=batch_size, drop_last=True, num_workers=4)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, drop_last=True, num_workers=2)
+val_loader = DataLoader(val_dataset, shuffle=False, batch_size=batch_size, drop_last=True, num_workers=2)
 
 # init model
 
@@ -107,7 +109,7 @@ def train_VAE(model, train_loader, val_loader, learning_rate, epochs, plot_loss=
         loop_time = (end_time - start_time)/60 # in minutes
         print(f'Executed in {loop_time} minutes')
          
-    return model, losses
+    return model
 
 def evaluate(model, val_loader):
     model.eval()
@@ -122,5 +124,5 @@ def evaluate(model, val_loader):
     avg_loss = epoch_loss / len(val_loader)
     return avg_loss
 
-cvae, losses = train_VAE(model, train_loader, val_loader, learning_rate, 
-                          epochs=10, plot_loss=False)
+vae = train_VAE(model, train_loader, val_loader, learning_rate, 
+                          epochs=EPOCHS, plot_loss=False)
