b134523050aa22189224faffa68de6a329e09336
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-07-15 12:44:56 +0200

    Update data.py

--- src/utils/data.py
+++ src/utils/data.py
@@ -9,21 +9,21 @@ and all of the molecules from the training set
 
 input: mol, molecule
 returns: float, highest tanimoto score when compared with
-                all the molecules from thre training set.
+                all the molecules from the training set.
 if returnSmiles:
 returns: float, highest tanimoto score
          string, smiles of the molecule with the highest tanimoto score in train
 '''
 
 def closest_in_train(mol, returnSmiles=False):
-    datapath = 'data/GRU_data/train_morgan_512bits.parquet'
+    data_path = 'data/GRU_data/train_morgan_512bits.parquet'
     high_tan = 0
     high_idx = 0
-    train_morgan_fps = pd.read_parquet(datapath).fps.apply(eval).tolist()
+    train_morgan_fps = pd.read_parquet(data_path).fps.apply(eval).tolist()
     query_fp = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=512)
+
     for idx, fp in enumerate(train_morgan_fps):
-        fp = src.utils.finger.dense2sparse(fp, fp_len=512)
-        fp = fp.tolist()
+        fp = src.utils.finger.dense2sparse(fp, fp_len=512).tolist()
         bitstring = ''.join([str(x) for x in fp])
         ebv = DataStructs.CreateFromBitString(bitstring)
         tan = DataStructs.TanimotoSimilarity(query_fp, ebv)
@@ -41,7 +41,7 @@ def closest_in_train(mol, returnSmiles=False):
 get smiles of a molecule by idx in the training set
 '''
 def get_smiles_from_train(idx):
-    datapath = 'data/GRU_data/train_dataset.parquet'
-    train = pd.read_parquet(datapath).smiles
+    data_path = 'data/GRU_data/train_dataset.parquet'
+    train = pd.read_parquet(data_path).smiles
     smiles = train.iloc[idx]
     return(smiles)
