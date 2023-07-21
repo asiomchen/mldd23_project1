8661fa5b36215eb8ebab9a866e788ec7625da267
M-Iwan <mateusz.iwan135@op.pl>
2023-07-21 11:03:40 +0200

    Update data.py
    Optimisation of code for calculating tanimoto similarity

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,53 +1,83 @@
 import pandas as pd
-import rdkit.Chem as Chem
-import rdkit.DataStructs as DataStructs
-import src.utils.finger
+from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.Chem.rdMolDescriptors import GetMorganFingerprintAsBitVect
 
 
-def closest_in_train(mol, returnSmiles=False):
+def load_data():
     """
-    Returns the highest tanimoto score between given molecule
-    and all the molecules from the training set
-
-    Args:
-        mol (rdkit.Chem.rdchem.Mol): molecule to compare
+    Loads the dataset needed for comparison
 
     Returns:
-        high_tan (float): highest tanimoto score
-
-    If returnSmiles is True, returns the highest tanimoto score
-    and SMILES of the molecule with the highest tanimoto score (slow)
+        train_morgan_fps (pd.DataFrame): df containing processed train dataset
     """
     data_path = 'data/GRU_data/train_morgan_512bits.parquet'
-    high_tan = 0
-    high_idx = 0
     train_morgan_fps = pd.read_parquet(data_path).fps.apply(eval).tolist()
-    query_fp = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=512)
+    return train_morgan_fps
 
-    for idx, fp in enumerate(train_morgan_fps):
-        fp = src.utils.finger.dense2sparse(fp, fp_len=512).tolist()
-        bitstring = ''.join([str(x) for x in fp])
-        ebv = DataStructs.CreateFromBitString(bitstring)
-        tan = DataStructs.TanimotoSimilarity(query_fp, ebv)
-        if tan > high_tan:
-            high_tan, high_idx = tan, idx
 
-    if returnSmiles:
-        high_smiles = get_smiles_from_train(high_idx)
-        return high_tan, high_smiles
-    else:
-        return high_tan
+def fp2bitstring(fp):
+    """
+    Changes the molecules fingerprint into a bitstring.
+
+    Args:
+        fp (list): list containing active bits of a vector
+
+    Returns:
+        bitstring (str): bitstring vector
+
+    """
+    bitstring = ['0'] * 512
+    for x in fp:
+        bitstring[x] = '1'
+    return ''.join(bitstring)
 
 
 def get_smiles_from_train(idx):
     """
     Returns smiles of a molecule by idx in the training set
+
     Args:
         idx (int): index of the molecule in the training set
+
     Returns:
         smiles (str): SMILES of the molecule
     """
+
     data_path = 'data/GRU_data/train_dataset.parquet'
     train = pd.read_parquet(data_path).smiles
     smiles = train.iloc[idx]
-    return (smiles)
+    return smiles
+
+
+def closest_in_train(mol, train_morgan_fps, return_smiles=False):
+    """
+    Returns the highest tanimoto score between given molecule
+    and all the molecules from the training set
+
+    Args:
+        mol (rdkit.Chem.Mol): molecule to compare
+        train_morgan_fps (pd.DataFrame): dataframe containing dataset to compare against
+        return_smiles (bool): marks whether to return the smiles of molecule as well
+
+    Returns:
+        high_tan (float): highest tanimoto score
+
+    """
+
+    high_tan = 0
+    high_idx = 0
+    query_fp = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=512)
+
+    for idx, fp in enumerate(train_morgan_fps):
+        bitstring = fp2bitstring(fp)
+        ebv = DataStructs.CreateFromBitString(bitstring)
+        tan = DataStructs.TanimotoSimilarity(query_fp, ebv)
+        if tan > high_tan:
+            high_tan, high_idx = tan, idx
+
+    if return_smiles:
+        high_smiles = get_smiles_from_train(high_idx)
+        return high_tan, high_smiles
+    else:
+        return high_tan
