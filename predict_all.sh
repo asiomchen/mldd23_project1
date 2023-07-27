4d510373d41f9f37aa776c5632642bc81e3ddd04
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-07-27 23:34:53 +0200

    update prediction script

--- predict_all.sh
+++ predict_all.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+#SBATCH --job-name=predictions
+#SBATCH --partition=student
+#SBATCH --qos=normal
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
 
+source ~/miniconda3/bin/activate
+conda init bash
+conda activate mldd
+
+python fp_sampler.py -t 5ht1a -n 100000
+python fp_sampler.py -t 5ht7 -n 100000
+python fp_sampler.py -t d2 -n 100000
+python fp_sampler.py -t beta2 -n 100000
+python fp_sampler.py -t h1 -n 100000
 python predict.py -c pred_config.ini
