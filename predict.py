35a43e2b04fcce9d4b32c46a373d1ca383c987ad
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-07-28 11:25:59 +0200

    restructure predict script

--- predict.py
+++ predict.py
@@ -5,11 +5,10 @@ import selfies as sf
 from torch.utils.data import DataLoader
 import rdkit.Chem as Chem
 import rdkit.Chem.Draw as Draw
-from rdkit.Chem import QED
+from src.pred.filter import molecule_filter
 import os
 import torch
 import pandas as pd
-from src.utils.data import closest_in_train
 import configparser
 import argparse
 from tqdm import tqdm
@@ -31,15 +30,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', type=str, default='pred_config.ini', help='Path to config file')
     config_path = parser.parse_args().config
-    torch.cuda.empty_cache()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using {device} device')
 
     # load config
     config = configparser.ConfigParser()
     config.read(config_path)
-    QED_threshold = float(config['FILTER']['QED_threshold'])
-    max_ring_size = int(config['FILTER']['max_ring_size'])
-    tanimoto = config['SCRIPT'].getboolean('tanimoto')
     batch_size = int(config['MODEL']['batch_size'])
     fp_len = int(config['MODEL']['fp_len'])
     encoding_size = int(config['MODEL']['encoding_size'])
@@ -63,8 +59,11 @@ def main():
     print(f'Loaded model from {model_path}')
 
     # get list of files and dirs in results folder
-    dir_list = os.listdir('results')
-    files = [name for name in dir_list if name.split('.')[-1] == 'parquet']
+    if os.path.isdir('results') and os.listdir('results') is not None:
+        dir_list = os.listdir('results')
+        files = [name for name in dir_list if name.split('.')[-1] == 'parquet']
+    else:
+        files = []
 
     # iterate over parquet files
     for name in files:
@@ -73,29 +72,24 @@ def main():
         f_name += '_done'
 
         # load data
-        df = pd.read_parquet(f'results/{name}')
+        data = pd.read_parquet(f'results/{name}')
 
         # get predictions
         print(f'Getting predictions for file {name}...')
         predictions = get_predictions(model,
-                                      df,
+                                      data,
                                       fp_len=fp_len,
                                       batch_size=batch_size,
                                       progress_bar=progress_bar
                                       )
 
-        # filter out non-druglike molecules
+        # pred out non-druglike molecules
         print('Filtering out non-druglike molecules...')
-        druglike_df = druglike_only(predictions,
-                                    df.fps,
-                                    QED_threshold,
-                                    max_ring_size
-                                    )
+        df = pd.DataFrame({'smiles': predictions})
+        df['mols'] = df.smiles.apply(Chem.MolFromSmiles)
+        df['raw_fps'] = data.fps
 
-        # find tanimoto scores
-        if tanimoto:
-            druglike_df = tanimoto_to_train(druglike_df, progress_bar=progress_bar)
-        druglike_df['smiles'] = druglike_df.mols.apply(Chem.MolToSmiles)
+        druglike_df = molecule_filter(df, config=config)
         output = druglike_df.drop(columns=['mols'])
 
         # save data as csv
@@ -114,7 +108,8 @@ def main():
         for i, mol in enumerate(druglike_df.mols):
             Draw.MolToFile(mol, f'results/{f_name}/imgs/{i}.png', size=(300, 300))
         time_elapsed = time() - start_time
-        print(f'Finished in {(time_elapsed/60):.2f} minutes')
+        print(f'Finished in {(time_elapsed / 60):.2f} minutes')
+
 
 def get_predictions(model, df, fp_len=4860, batch_size=512, progress_bar=False):
     """
@@ -146,46 +141,6 @@ def get_predictions(model, df, fp_len=4860, batch_size=512, progress_bar=False):
                     preds_smiles.append('C')  # dummy SMILES
                     print('DecoderError raised, appending dummy SMILES')
     return preds_smiles
-
-
-def druglike_only(predictions, fps, threshold, max_ring_size):
-    """
-    Filters out non-druglike molecules from a list of SMILES.
-    Args:
-        predictions: List of SMILES.
-        fps: List of fingerprints.
-        threshold (float): QED threshold.
-        max_ring_size (int): Max ring size.
-
-    Returns:
-    pd.DataFrame: Dataframe containing druglike molecules.
-    """
-    # convert SMILES to mols and calculate QED
-    df = pd.DataFrame({'smiles': predictions})
-    df['mols'] = df.smiles.apply(Chem.MolFromSmiles)
-    df['qeds'] = df.mols.apply(Chem.QED.qed)
-    df['raw_fps'] = fps
-
-    # filter by QED
-    df = df[df.qeds > threshold]
-    # filter by max ring size
-    df = df[df.mols.apply(get_largest_ring) <= max_ring_size]
-    return df
-
-
-def get_largest_ring(mol):
-    ri = mol.GetRingInfo()
-    rings = []
-    for b in mol.GetBonds():
-        ring_len = [len(ring) for ring in ri.BondRings() if b.GetIdx() in ring]
-        rings += ring_len
-    return max(rings) if rings else 0
-
-
-def tanimoto_to_train(df, progress_bar=False):
-    df['tanimoto'] = [closest_in_train(mol) for mol in tqdm(df.mols, disable=not progress_bar)]
-    return df
-
 
 if __name__ == '__main__':
     main()
