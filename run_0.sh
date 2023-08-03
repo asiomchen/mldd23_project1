f1c324d0e1edfee8d6efbf10704a08d7b334a149
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-03 16:38:06 +0200

    reformat config files

--- run_0.sh
+++ run_0.sh
@@ -6,6 +6,4 @@ echo "training VAE"
 python -u train_vae.py -c v_0.ini > log_vae_0.out
 echo "training GRU"
 python -u train_gru.py -c g_0.ini > log_gru_0.out
-echo "training GRU with RL"
-python -u RL.py -c rl_0.ini > log_rl_0.out
 echo "run finished"
