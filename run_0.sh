88bd144b0cb6dc73b14631329a9cb1223305e425
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-04 18:29:04 +0200

    add wandb login

--- run_0.sh
+++ run_0.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ~/miniconda3/bin/activate
 conda init bash
-conda activate mldd
+conda activate
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
 echo "training VAE"
 python -u train_vae.py -c v_0.ini > log_vae_0.out
 echo "training GRU"
--- run_1.sh
+++ run_1.sh
@@ -2,6 +2,7 @@
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
 echo "training VAE"
 python -u train_vae.py -c v_1.ini > log_vae_1.out
 echo "training GRU"
