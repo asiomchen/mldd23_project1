f006caaca1835f5e87158b2edf88f015a59e430f
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-05 16:00:49 +0200

    fix gru config files

--- run_0.sh
+++ run_0.sh
@@ -3,8 +3,6 @@ source ~/miniconda3/bin/activate
 conda init bash
 conda activate
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-echo "training VAE"
-python -u train_vae.py -c v_0.ini > log_vae_0.out
 echo "training GRU"
 python -u train_gru.py -c g_0.ini > log_gru_0.out
 echo "run finished"
--- run_1.sh
+++ run_1.sh
@@ -3,8 +3,6 @@ source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-echo "training VAE"
-python -u train_vae.py -c v_1.ini > log_vae_1.out
 echo "training GRU"
 python -u train_gru.py -c g_1.ini > log_gru_1.out
 echo "run finished"
