5211df09003bec60efaeb18dae7e647688aa6b51
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-08 22:30:38 +0200

    prepare for train

--- train_vae5.sh
+++ train_vae5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=5ht1a_vae
+#SBATCH --job-name=VAE5
 #SBATCH --partition=student
-#SBATCH --qos=big
+#SBATCH --qos=quick
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=32G
@@ -11,5 +11,5 @@ source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
 wandb login
-python -u train_vae.py > log_vae.out
+python -u train_vae.py -c vae5.ini > log_vae5.out
 echo done
