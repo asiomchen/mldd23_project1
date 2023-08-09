4b5b2f7237f615b0d3b2db0b461cb4c60e92117f
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-09 11:30:46 +0200

    fix annealing

--- src/utils/annealing.py
+++ src/utils/annealing.py
@@ -7,33 +7,34 @@ class Annealing:
     After each call, the step() function should be called to update the current epoch.
     Parameters:
         epochs (int): Number of epochs to train the VAE.
-        slope (str): Slope of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
+        shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
     Args:
         kld (torch.Tensor): KL divergence loss.
     Returns:
         torch.Tensor: Annealed KL divergence loss.
     """
 
-    def __init__(self, epochs: int, slope='linear'):
+    def __init__(self, epochs: int, shape='linear'):
         self.epochs = epochs
         self.current_epoch = 1
-        self.slope = slope
+        self.shape = shape
         self.pi = torch.tensor(3.14159265359)
 
     def __call__(self, kld):
+        out = kld * self.slope()
+        return out
+
+    def slope(self):
+        slope = torch.tensor(0)
         if self.slope == 'linear':
-            output = kld * (self.current_epoch / self.epochs)
-            return output
+            slope = (self.current_epoch / self.epochs)
         elif self.slope == 'cosine':
-            cosine_slope = 0.5 + 0.5 * torch.cos(self.pi * self.current_epoch / self.epochs - self.pi)
-            output = kld * cosine_slope
-            return output
+            slope = 0.5 + 0.5 * torch.cos(self.pi * self.current_epoch / self.epochs - self.pi)
         elif self.slope == 'logistic':
             smoothness = 5
             exponent = ((self.epochs / 2) - self.current_epoch) / smoothness
-            sigmoid_slope = 1 / (1 + torch.exp(exponent))
-            output = kld * sigmoid_slope
-            return output
+            slope = 1 / (1 + torch.exp(exponent))
+        return slope
 
     def step(self):
         if self.current_epoch < self.epochs:
