03659d05d5b5bf05baa7b9222a3f02d1798c1089
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-10 13:23:46 +0200

    remove redundant files

--- run_4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ~/miniconda3/bin/activate
-conda init bash
-conda activate mldd
-wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-echo "training GRU"
-python -u train_gru.py -c g_4.ini > log_gru_4.out
-echo "run finished"
