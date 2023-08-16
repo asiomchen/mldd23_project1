d8be21a5bbf7286cd2d013f416a877a5f22851c6
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-08-16 19:19:02 +0200

    add training scripts

--- train_gru0.sh
+++ train_gru0.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=GRU_512_nograd_encoder
+#SBATCH --job-name=GRU_0
 #SBATCH --partition=student
 #SBATCH --qos=big
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=32G
 
-nvidia-smi -L
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python -u train_gru.py -c gru0.ini > log_gru_512.out
+python -u train_gru.py -c gru_config0.ini > log_gru0.out
 echo done
--- train_gru1.sh
+++ train_gru1.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=VAE4
+#SBATCH --job-name=GRU_1
 #SBATCH --partition=student
-#SBATCH --qos=quick
+#SBATCH --qos=big
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=32G
 
-nvidia-smi -L
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
-wandb login
-python -u train_vae.py -c vae64.ini > log_vae64.out
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
+python -u train_gru.py -c gru_config1.ini > log_gru1.out
 echo done
