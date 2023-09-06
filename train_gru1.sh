8e771947f39456a3cdd807eaa7f3deef771e1db2
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-06 10:51:36 +0200

    add training scripts for gru

---
+++ train_gru1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=GRUv3_1
+#SBATCH --partition=student
+#SBATCH --qos=big
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem-per-cpu=32G
+
+nvidia-smi -L
+source ~/miniconda3/bin/activate
+conda init bash
+conda activate mldd
+wandb login
+python -u train_gru.py -c gru_config1.ini > log_gru1.out
+echo done
