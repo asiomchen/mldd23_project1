6e6cb3c253d4ffe3ff46ad1b5b7744cba8e49b07
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-12 12:42:42 +0200

    Update get_datasets.sh

--- get_datasets.sh
+++ get_datasets.sh
@@ -2,15 +2,14 @@
 cd data
 mkdir train_data
 cd train_data
-# wget -O combined_dataset.parquet https://www.dropbox.com/s/9nsvjcvfvpamjez/combined_dataset.parquet?dl=1
-wget -O combined_dataset_ECFP.parquet https://www.dropbox.com/scl/fi/azg7tk8mtuap65zy6a9h1/combined_dataset_ECFP.parquet?rlkey=4m3zailmm4rag540b3son1oq6&dl=1
-# wget -O train_morgan_512bits.parquet https://www.dropbox.com/s/bj37jlql9j1lcrr/train_morgan_512bits.parquet?dl=1
-# cd -
-# mkdir interactions
-# cd interactions
-# wget -O 5ht1a_dense.parquet https://www.dropbox.com/s/lcwn20vlc2xq5i4/5ht1a_dense.parquet?dl=1
-# wget -O 5ht7_dense.parquet https://www.dropbox.com/s/pg42p1jqy4ghrwi/5ht7_dense.parquet?dl=1
-# wget -O beta2_dense.parquet https://www.dropbox.com/s/1zsekovgsy2vfik/beta2_dense.parquet?dl=1
-# wget -O d2_dense.parquet https://www.dropbox.com/s/fb4tgn92wgshs05/d2_dense.parquet?dl=1
-# wget -O h1_dense.parquet https://www.dropbox.com/s/wt14xi0elqxqa08/h1_dense.parquet?dl=1
+wget -O combined_dataset.parquet https://www.dropbox.com/s/9nsvjcvfvpamjez/combined_dataset.parquet?dl=1
+wget -O train_morgan_512bits.parquet https://www.dropbox.com/s/bj37jlql9j1lcrr/train_morgan_512bits.parquet?dl=1
+cd -
+mkdir interactions
+cd interactions
+wget -O 5ht1a_dense.parquet https://www.dropbox.com/s/lcwn20vlc2xq5i4/5ht1a_dense.parquet?dl=1
+wget -O 5ht7_dense.parquet https://www.dropbox.com/s/pg42p1jqy4ghrwi/5ht7_dense.parquet?dl=1
+wget -O beta2_dense.parquet https://www.dropbox.com/s/1zsekovgsy2vfik/beta2_dense.parquet?dl=1
+wget -O d2_dense.parquet https://www.dropbox.com/s/fb4tgn92wgshs05/d2_dense.parquet?dl=1
+wget -O h1_dense.parquet https://www.dropbox.com/s/wt14xi0elqxqa08/h1_dense.parquet?dl=1
 echo All datasets downloaded successfully
