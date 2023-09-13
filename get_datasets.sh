c82495d023dbf66b0442efd994d5f9fce413c643
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-13 15:29:58 +0200

    Update get_datasets.sh

--- get_datasets.sh
+++ get_datasets.sh
@@ -3,7 +3,15 @@ cd data
 mkdir train_data
 cd train_data
 wget -O big_dataset.parquet https://www.dropbox.com/scl/fi/kbrlo5hqvpezmp0v4nlm9/big_dataset.parquet?rlkey=0f28kmf2vy9gs9k57fagdu4k5&dl=1
-# wget -O train_morgan_512bits.parquet https://www.dropbox.com/s/bj37jlql9j1lcrr/train_morgan_512bits.parquet?dl=1
+wget -O train_morgan_512bits.parquet https://www.dropbox.com/s/bj37jlql9j1lcrr/train_morgan_512bits.parquet?dl=1
+cd -
+mkdir activity_data
+cd activity_data
+wget -O 5ht1a_klek_100nM.parquet https://www.dropbox.com/scl/fi/lsxxodjc596x8hvd0qoe1/5ht1a_klek_100nM.parquet?rlkey=2yay5wgpdupanxe9xatngxaqy&dl=1
+wget -O 5ht7_klek_100nM.parquet https://www.dropbox.com/scl/fi/rsqjoza241xls5w3qae26/5ht7_klek_100nM.parquet?rlkey=rses4n02n8h9f7kgg5k0o80vr&dl=1
+wget -O beta2_klek_100nM.parquet https://www.dropbox.com/scl/fi/hy1dcderq2xj1zhwdqyz3/beta2_klek_100nM.parquet?rlkey=394al0sfeyrhstfeb5d879tou&dl=1
+wget -O d2_klek_100nM.parquet https://www.dropbox.com/scl/fi/srqqwjyfmntiiuxfzf3cf/d2_klek_100nM.parquet?rlkey=a8vi1brbilfl2oelxcac0ac5f&dl=1
+wget -O h1_klek_100nM.parquet https://www.dropbox.com/scl/fi/qxp3q5gnins6tgzjfnfol/h1_klek_100nM.parquet?rlkey=q4dsjg79429p96x7d7kn6k38p&dl=1
 # cd -
 # mkdir interactions
 # cd interactions
