15f81b45e855ad66ed93d9435696e09cf601fdf0
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-14 22:40:01 +0200

    add dropout prediction

--- predict.py
+++ predict.py
@@ -6,14 +6,14 @@ import queue
 import time
 import pandas as pd
 import rdkit.Chem.Draw as Draw
-import selfies as sf
+import rdkit.Chem as Chem
+from src.pred.droputpred import predict_with_dropout
 import torch
 from src.gen.generator import EncoderDecoderV3
 from src.pred.filter import molecule_filter
-from src.utils.vectorizer import SELFIESVectorizer
 
 
-def predict(file_path, model_path, config_path, is_verbose=True):
+def predict(file_path, model_path, config_path, is_verbose=True, use_cuda=True):
     """
     Predicting molecules using the trained model.
 
@@ -22,6 +22,7 @@ def predict(file_path, model_path, config_path, is_verbose=True):
         model_path (str): Path to the model weights.
         config_path: Path to the config file.
         is_verbose (bool): Whether to print progress.
+        use_cuda (bool): Whether to use GPU for prediction (VRAM memory intensive).
     Returns: None
     """
 
@@ -32,7 +33,6 @@ def predict(file_path, model_path, config_path, is_verbose=True):
     name = file_path.split('/')[-1].split('.')[0]
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     name = '_'.join([name, timestamp])
-    use_cuda = False
     device = 'cuda' if use_cuda and torch.cuda.is_available() else 'cpu'
     print(f'Using {device} device') if is_verbose else None
 
@@ -47,7 +47,7 @@ def predict(file_path, model_path, config_path, is_verbose=True):
         encoding_size=int(config['MODEL']['encoding_size']),
         hidden_size=int(config['MODEL']['hidden_size']),
         num_layers=int(config['MODEL']['num_layers']),
-        dropout=0.0,
+        dropout=0.1,
         teacher_ratio=0.0,
         use_cuda=use_cuda,
         output_size=42,
@@ -57,7 +57,7 @@ def predict(file_path, model_path, config_path, is_verbose=True):
         fc3_size=int(config['MODEL']['fc3_size'])
     ).to(device)
 
-    model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
+    model.load_state_dict(torch.load(model_path, map_location=device))
     print(f'Loaded model from {model_path}') if is_verbose else None
 
     config.read(config_path)
@@ -67,15 +67,15 @@ def predict(file_path, model_path, config_path, is_verbose=True):
     for col in ['smiles', 'label', 'score']:
         if col in query_df.columns:
             query_df = query_df.drop(columns=[col])
-    input_tensor = torch.tensor(query_df.to_numpy(), dtype=torch.float32)
+    input_vector = query_df.to_numpy()
 
     # get predictions
     print(f'Getting predictions for file {file_path}...') if is_verbose else None
-    df = get_predictions(model,
-                         input_tensor,
-                         use_cuda=use_cuda,
-                         verbose=is_verbose
-                         )
+    df = predict_with_dropout(model,
+                              input_vector,
+                              n_iter=int(config['SCRIPT']['n_iter']),
+                              device=device,
+                              return_imgs=False)
 
     # pred out non-druglike molecules
     print('Filtering out non-druglike molecules...') if is_verbose else None
@@ -125,10 +125,8 @@ def predict(file_path, model_path, config_path, is_verbose=True):
 
     # save data as csv
     os.mkdir(f'results/{name}')
-
     with open(f'results/{name}/config.ini', 'w') as configfile:
         config.write(configfile)
-
     druglike_df.to_csv(f'results/{name}/{name}.csv',
                        columns=['smiles', 'qed'],
                        index=False)
@@ -137,45 +135,12 @@ def predict(file_path, model_path, config_path, is_verbose=True):
 
     # save images
     os.mkdir(f'results/{name}/imgs')
-
-    for i, mol in enumerate(druglike_df.mols):
+    for i, smiles in enumerate(druglike_df['smiles']):
+        mol = Chem.MolFromSmiles(smiles)
         Draw.MolToFile(mol, f'results/{name}/imgs/{i}.png', size=(300, 300))
 
     time_elapsed = time.time() - start_time
     print(f'{name} processed in {(time_elapsed / 60):.2f} minutes')
-
-
-def get_predictions(model,
-                    input_tensor,
-                    use_cuda=False,
-                    verbose=True,
-                    ):
-    """
-    Generates predictions for a given model and dataframe.
-    Args:
-        model (nn.Module): Model to use for predictions.
-        input_tensor (torch.Tensor) Tensor containing batched latent vectors.
-        use_cuda (bool): Whether to use CUDA for predictions.
-        verbose (bool): Whether to print progress.
-    Returns:
-        output (pd.DataFrame): prediction df containing 'smiles' and 'fp' columns
-    """
-    device = 'cuda' if use_cuda and torch.cuda.is_available() else 'cpu'
-    vectorizer = SELFIESVectorizer(pad_to_len=128)
-    preds_smiles = []
-    with torch.no_grad():
-        X = input_tensor.to(device)
-        preds, _ = model(X, None, teacher_forcing=False, omit_encoder=True)
-        preds = preds.cpu().numpy()
-        for seq in preds:
-            selfie = vectorizer.devectorize(seq, remove_special=True)
-            try:
-                preds_smiles.append(sf.decoder(selfie))
-            except sf.DecoderError:
-                preds_smiles.append('C')  # dummy SMILES
-    print('Predictions complete') if verbose else None
-    output = pd.DataFrame({'smiles': preds_smiles})
-    return output
 
 
 if __name__ == '__main__':
