a5dfe969143820012b1f313e276948f6b3f950b4
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-14 16:42:53 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,55 +4,42 @@ import multiprocessing as mp
 import os
 import queue
 import time
-
 import pandas as pd
 import rdkit.Chem.Draw as Draw
-import rdkit.Chem as Chem
 import selfies as sf
 import torch
-
 from src.gen.generator import EncoderDecoderV3
 from src.pred.filter import molecule_filter
 from src.utils.vectorizer import SELFIESVectorizer
 
 
-def predict(file_path, is_verbose=True):
+def predict(file_path, model_path, config_path, is_verbose=True):
     """
     Predicting molecules using the trained model.
 
     Args:
         file_path (str): Path to the file containing latent vectors.
+        model_path (str): Path to the model weights.
+        config_path: Path to the config file.
         is_verbose (bool): Whether to print progress.
     Returns: None
     """
 
     # setup
     start_time = time.time()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config',
-                        '-c',
-                        type=str,
-                        default='config_files/pred_config.ini',
-                        help='Path to config file')
-    config_path = parser.parse_args().config
 
     # get file name
-    dir_name, file_name = file_path.split('/')
-    name, _ = file_name.split('.')
+    name = file_path.split('/')[-1].split('.')[0]
     timestamp = time.strftime("%Y%m%d-%H%M%S")
-    name = '_'.join([dir_name, name, timestamp])
-
-    # load config
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    model_path = str(config['MODEL']['model_path'])
-    use_cuda = config['SCRIPT'].getboolean('cuda')
+    name = '_'.join([name, timestamp])
+    use_cuda = False
     device = 'cuda' if use_cuda and torch.cuda.is_available() else 'cpu'
     print(f'Using {device} device') if is_verbose else None
 
-    if model_path.lower() == 'auto':
-        model_config_path = f'models/{dir_name}/hyperparameters.ini'
-        config.read(model_config_path)
+    model_epoch = model_path.split('/')[-1]
+    model_config_path = model_path.replace(model_epoch, 'hyperparameters.ini')
+    config = configparser.ConfigParser()
+    config.read(model_config_path)
 
     # load model
     model = EncoderDecoderV3(
@@ -70,20 +57,14 @@ def predict(file_path, is_verbose=True):
         fc3_size=int(config['MODEL']['fc3_size'])
     ).to(device)
 
-    if model_path.lower() == 'auto':
-        info = configparser.ConfigParser()
-        info.read(f'data/encoded_data/{dir_name}/info.ini')
-        model_path = info['INFO']['model_path']
-        model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
+    print(f'Loaded model from {model_path}') if is_verbose else None
 
-    elif model_path.lower() != 'auto':
-        model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
-        print(f'Loaded model from {model_path}') if is_verbose else None
+    config.read(config_path)
 
     # load data
-    query_df = pd.read_parquet(f'data/encoded_data/{dir_name}/{file_name}')
-    target_smiles = query_df['smiles'].values
-    for col in ['smiles', 'label']:
+    query_df = pd.read_parquet(file_path)
+    for col in ['smiles', 'label', 'score']:
         if col in query_df.columns:
             query_df = query_df.drop(columns=[col])
     input_tensor = torch.tensor(query_df.to_numpy(), dtype=torch.float32)
@@ -95,7 +76,6 @@ def predict(file_path, is_verbose=True):
                          use_cuda=use_cuda,
                          verbose=is_verbose
                          )
-    df['target_smiles'] = target_smiles
 
     # pred out non-druglike molecules
     print('Filtering out non-druglike molecules...') if is_verbose else None
@@ -109,7 +89,10 @@ def predict(file_path, is_verbose=True):
     q = queue.Queue()
 
     # prepare a process for each file and add to queue
-    chunk_size = len(df) // cpus + 1 if len(df) % cpus != 0 else len(df) // cpus
+    if len(df) < 5000:
+        chunk_size = 20
+    else:
+        chunk_size = 1000
     n_chunks = int(len(df) / chunk_size) + 1 if len(df) % chunk_size != 0 else int(len(df) / chunk_size)
     chunks = [df[i * chunk_size:(i + 1) * chunk_size] for i in range(n_chunks)]
     for chunk in chunks:
@@ -124,8 +107,9 @@ def predict(file_path, is_verbose=True):
             break
         if len(mp.active_children()) < cpus:
             proc = q.get()
-            print("(mp) Starting:", proc.name)
             proc.start()
+            if q.qsize() % 5 == 0:
+                print('(mp) Processes in queue: ', q.qsize())
             processes.append(proc)
         time.sleep(1)
 
@@ -154,30 +138,8 @@ def predict(file_path, is_verbose=True):
     # save images
     os.mkdir(f'results/{name}/imgs')
 
-    if 'target_smiles' in druglike_df.columns:
-        if 'tanimoto' in druglike_df.columns:
-            for i, tuple in enumerate(zip(druglike_df.mols, druglike_df.target_smiles, druglike_df.tanimoto)):
-                mol, target_smile, tanimoto = tuple
-                target_mol = Chem.MolFromSmiles(target_smile)
-                img = Draw.MolsToGridImage([mol, target_mol],
-                                           legends=[f'pred', f'target\nclosest in train: {tanimoto}'],
-                                           molsPerRow=2,
-                                           subImgSize=(300, 300)
-                                           )
-                img.save(f'results/{name}/imgs/{i}.png')
-        else:
-            for i, tuple in enumerate(zip(druglike_df.mols, druglike_df.target_smiles)):
-                mol, target_smile = tuple
-                target_mol = Chem.MolFromSmiles(target_smile)
-                img = Draw.MolsToGridImage([mol, target_mol],
-                                           legends=['pred', 'target'],
-                                           molsPerRow=2,
-                                           subImgSize=(300, 300)
-                                           )
-                img.save(f'results/{name}/imgs/{i}.png')
-    else:
-        for i, mol in enumerate(druglike_df.mols):
-            Draw.MolToFile(mol, f'results/{name}/imgs/{i}.png', size=(300, 300))
+    for i, mol in enumerate(druglike_df.mols):
+        Draw.MolToFile(mol, f'results/{name}/imgs/{i}.png', size=(300, 300))
 
     time_elapsed = time.time() - start_time
     print(f'{name} processed in {(time_elapsed / 60):.2f} minutes')
@@ -217,19 +179,20 @@ def get_predictions(model,
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config',
+                        '-c',
+                        type=str,
+                        default='config_files/pred_config.ini',
+                        help='Path to config file')
+    parser.add_argument('-d',
+                        '--data',
+                        type=str,
+                        help='Path to data file')
+    parser.add_argument('-m',
+                        '--model_path',
+                        type=str,
+                        help='Path to model weights')
+    args = parser.parse_args()
+    predict(file_path=args.data, model_path=args.model_path, config_path=args.config)
 
-    # get list of files and dirs in data/encoded_data directory
-    if not os.path.isdir('data/encoded_data'):
-        os.mkdir('data/encoded_data')
-
-    encoded_data_directories = os.listdir('data/encoded_data')
-    parquet_files = []
-    for dir_name in encoded_data_directories:
-        dir_list = os.listdir(f'data/encoded_data/{dir_name}')
-        files = [f'{dir_name}/{name}' for name in dir_list if name.split('.')[-1] == 'parquet']
-        parquet_files.extend(files)
-    if not parquet_files:
-        print('No .parquet files found')
-
-    for file in parquet_files:
-        predict(file, is_verbose=True)
