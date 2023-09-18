0f0f9af95f9b3f2ac6080ba42671ff2451968ee7
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-18 13:09:10 +0200

    Update get_datasets.sh

--- get_datasets.sh
+++ get_datasets.sh
@@ -2,17 +2,17 @@
 cd data
 mkdir train_data
 cd train_data
-# wget -O big_dataset_std.parquet https://www.dropbox.com/scl/fi/rdhb58364f38isl2v9mf7/big_dataset_std.parquet?rlkey=rotv0zdtxk8gcxy9o2kzxwk9e&dl=1
+wget -O big_dataset_std.parquet https://www.dropbox.com/scl/fi/mwohd2i25m7xcygaay3hr/big_dataset_std.parquet?rlkey=zaq8er9ez0ripvc4jm0u0vr1y&dl=1
 # wget -O big_dataset.parquet https://www.dropbox.com/scl/fi/kbrlo5hqvpezmp0v4nlm9/big_dataset.parquet?rlkey=0f28kmf2vy9gs9k57fagdu4k5&dl=1
 # wget -O train_morgan_512bits.parquet https://www.dropbox.com/s/bj37jlql9j1lcrr/train_morgan_512bits.parquet?dl=1
 cd -
 mkdir activity_data
 cd activity_data
- wget -O 5ht1a_klek_100nM.parquet https://www.dropbox.com/scl/fi/surb3lv87yrwcfcu6evn2/5ht1a_klek_100nM.parquet?rlkey=edqpteyt3m9e3cump9pguwvfa&dl=1
- wget -O 5ht7_klek_100nM.parquet https://www.dropbox.com/scl/fi/giuhzk3n191w1opy4bews/5ht7_klek_100nM.parquet?rlkey=9qs1u906u39rqm7u6kco3wldh&dl=1
- wget -O beta2_klek_100nM.parquet https://www.dropbox.com/scl/fi/4hvmt4wstyhtynghta22k/beta2_klek_100nM.parquet?rlkey=q6u6dbzva3iqx1sfs3mc22eda&dl=1
- wget -O d2_klek_100nM.parquet https://www.dropbox.com/scl/fi/b7xtwokibqcetlgdomuiy/d2_klek_100nM.parquet?rlkey=3v3uet9c5jov059zowds4h9a7&dl=1
- wget -O h1_klek_100nM.parquet https://www.dropbox.com/scl/fi/ctlumo9t6gf4zc1hrronl/h1_klek_100nM.parquet?rlkey=1e7z8a6alah4pdljbtw1378tl&dl=1
+# wget -O 5ht1a_klek_100nM.parquet https://www.dropbox.com/scl/fi/surb3lv87yrwcfcu6evn2/5ht1a_klek_100nM.parquet?rlkey=edqpteyt3m9e3cump9pguwvfa&dl=1
+# wget -O 5ht7_klek_100nM.parquet https://www.dropbox.com/scl/fi/giuhzk3n191w1opy4bews/5ht7_klek_100nM.parquet?rlkey=9qs1u906u39rqm7u6kco3wldh&dl=1
+# wget -O beta2_klek_100nM.parquet https://www.dropbox.com/scl/fi/4hvmt4wstyhtynghta22k/beta2_klek_100nM.parquet?rlkey=q6u6dbzva3iqx1sfs3mc22eda&dl=1
+# wget -O d2_klek_100nM.parquet https://www.dropbox.com/scl/fi/b7xtwokibqcetlgdomuiy/d2_klek_100nM.parquet?rlkey=3v3uet9c5jov059zowds4h9a7&dl=1
+# wget -O h1_klek_100nM.parquet https://www.dropbox.com/scl/fi/ctlumo9t6gf4zc1hrronl/h1_klek_100nM.parquet?rlkey=1e7z8a6alah4pdljbtw1378tl&dl=1
 # cd -
 # mkdir interactions
 # cd interactions
