09cd646ab8e57804cf87ebc7135bf15f3e4d2f16
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-19 09:30:54 +0200

    Update bayes.sh

--- bayes.sh
+++ bayes.sh
@@ -9,7 +9,7 @@ nvidia-smi -L
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
-python bayesian_search.py -m models/SVC_gelu_200_23-57-38/model.pkl -t sklearn -n 1000 -i 20 -b 4 > log_bayes.out
-python bayesian_search.py -m models/SVC_tapir_200_23-55-54/model.pkl -t sklearn -n 1000 -i 20 -b 4 > log_bayes.out
+python bayesian_search.py -m models/SVC_gelu_200_23-57-38/model.pkl -t sklearn -n 1000 -i 40 -b 4 -l 32 > log_bayes.out
+python bayesian_search.py -m models/SVC_tapir_200_23-55-54/model.pkl -t sklearn -n 1000 -i 40 -b 4 -l 64 > log_bayes.out
 
 echo done
