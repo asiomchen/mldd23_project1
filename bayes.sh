7281ee30b3a8f6f2f2f04734053f403956d23a79
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-19 09:28:52 +0200

    Update bayes.sh

--- bayes.sh
+++ bayes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=bayesian_search
 #SBATCH --partition=student
-#SBATCH --qos=normal
+#SBATCH --qos=quick
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=32G
 
@@ -9,5 +9,7 @@ nvidia-smi -L
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
-python bayesian_search.py -m models/discr_d2_tatra_epoch_80/epoch_150.pt -n 100 -i 100 > log_bayes.out
+python bayesian_search.py -m models/SVC_gelu_200_23-57-38/model.pkl -t sklearn -n 1000 -i 20 -b 4 > log_bayes.out
+python bayesian_search.py -m models/SVC_tapir_200_23-55-54/model.pkl -t sklearn -n 1000 -i 20 -b 4 > log_bayes.out
+
 echo done
