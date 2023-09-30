78db9c5fc7887fe3d586ffbbfb7f3ac51a3dbc13
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2023-09-30 15:05:38 +0200

    docking script update

--- docking/submit_jobs.py
+++ docking/submit_jobs.py
@@ -16,25 +16,25 @@ def main():
 
     df = pd.read_csv(args.data_path).sample(args.n_samples)
     df_list = np.split(df, n_chunks)
+    os.makedirs('docking/inputs', exist_ok=True)
     for n, df in enumerate(df_list):
         df.to_csv(f'docking/inputs/chunk_{n}.csv', index=False)
 
     # generate shell scripts and submit jobs
 
     for i in range(n_chunks):
-        with (open(f'docking/docking_{i}.sh', 'w') as f):
+        with open(f'docking/docking_{i}.sh', 'w') as f:
             header = [
                 f'''#!/bin/bash''',
                 f'''#SBATCH --job-name=docking_{i}''',
                 '''# SBATCH -N 1''',
                 '''# SBATCH --cpus-per-task=48''',
                 '''# SBATCH --mem-per-cpu=500MB''',
-                '''# SBATCH --time=72:00:00''',
+                '''# SBATCH --time=12:00:00''',
                 '''# SBATCH -A plgporphconj-cpu''',
                 '''# SBATCH -p plgrid''',
                 '''cd $SLURM_SUBMIT_DIR''',
-                '''conda''',
-                '''activate mldd\n'''
+                '''conda activate activate /net/pr2/projects/plgrid/plggjmdgroup/mldd\n'''
             ]
             f.write('\n'.join(header))
             f.write(f'''python docking/docking.py -d docking/inputs/chunk_{i}.csv\n''')
