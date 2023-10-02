69ac3416bc1440f73f0fcf6413338688325fe25e
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-10-02 12:09:49 +0200

    update scripts

--- bayes1.sh
+++ bayes1.sh
@@ -3,6 +3,6 @@
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
-python bayesian_search.py -m models/SVC_tails/model.pkl -n 10000 -i 40 -b 4 > log_bayes1.out
+python bayesian_search.py -m models/SVC_sonic/c1.pkl -n 10000 -i 40 -b 4 > log_bayes1.out
 
 echo done
--- bayes3.sh
+++ bayes3.sh
@@ -3,6 +3,6 @@
 source ~/miniconda3/bin/activate
 conda init bash
 conda activate mldd
-python bayesian_search.py -m models/SVC_knuckles/model.pkl -n 10000 -i 40 -b 4 > log_bayes3.out
+python bayesian_search.py -m models/SVC_tola_150/c1.pkl -n 10000 -i 40 -b 4 > log_bayes3.out
 
 echo done
