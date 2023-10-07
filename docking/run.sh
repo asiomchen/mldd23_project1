0906887bdcf332fbd45ae921bf21bee6449a3207
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-10-07 10:18:48 +0200

    Update run.sh

--- docking/run.sh
+++ docking/run.sh
@@ -8,5 +8,5 @@
 
 cd $SLURM_SUBMIT_DIR
 conda activate /net/pr2/projects/plgrid/plggjmdgroup/mldd
-python submit_jobs.py -ns 1000 -nc 10 -d results/SVC_lolek_150_18-28-52/preds_20230927-095252/predictions.csv
+python submit_jobs.py -ns 1000 -nc 10 -d results/5ht1a_SVC_20231003_152751/preds_20230927-095252/predictions.csv
 echo "Jobs submitted!"
