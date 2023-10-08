37a33d05cedc26eb06cf7664626b640bad983fbd
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2023-10-08 23:11:23 +0200

    manual input gen

---
+++ docking/docking_8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=docking_8
+#SBATCH -N 1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=500MB
+#SBATCH --time=24:00:00
+#SBATCH -A plgporphconj-cpu
+#SBATCH -p plgrid
+cd $SLURM_SUBMIT_DIR
+conda activate activate /net/pr2/projects/plgrid/plggjmdgroup/mldd
+python docking/docking.py -d docking/inputs/chunk_8.csv
+echo Done
