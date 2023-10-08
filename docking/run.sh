6e85362d6972eb9063b9436e4695533ab32e0c21
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-10-08 13:43:05 +0200

    add protein files

--- docking/run.sh
+++ docking/run.sh
@@ -8,5 +8,6 @@
 
 cd $SLURM_SUBMIT_DIR
 conda activate /net/pr2/projects/plgrid/plggjmdgroup/mldd
-python submit_jobs.py -ns 1000 -nc 10 -d results/5ht1a_SVC_20231003_152751/preds_20230927-095252/predictions.csv
+python submit_jobs.py -ns 1000 -nc 10 -d results/5ht1a_SVC_20231003_152751/preds_20231005-172945/predictions.csv
+python submit_jobs.py -ns 1000 -nc 10 -d results/c1_SVC_20231003_193344/preds_20231005-195627/predictions.csv
 echo "Jobs submitted!"
